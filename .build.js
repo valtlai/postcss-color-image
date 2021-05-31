c338f520e21c8480496a3a9359a938d815478d66
Valtteri Laitinen <dev@valtlai.fi>
2021-05-31 17:34:13 +0300

    Build Node.js version just before publishing

--- .build.js
+++ .build.js
@@ -1,27 +1,6 @@
-// $ deno run --allow-read=. --allow-write=. .build.js
+import build from "https://raw.githubusercontent.com/valtlai/nodefy/1.0.0/mod.js";
 
-await Promise.all([
-  Deno.readTextFile("index.js").then((input) => {
-    const denoOutput = input.replace(
-      'from "postcss-value-parser"',
-      'from "https://deno.land/x/postcss_value_parser@v4.1.0/mod.js"',
-    );
-
-    const cjsOutput = '"use strict";\n\n' + input
-      .replace(/(from "\.\/[^"]+\.)js"/, '$1cjs"')
-      .replace(/import ([\w\s{},]+) from ("[^"]+")/g, "const $1 = require($2)")
-      .replace("export default", "module.exports =");
-
-    return Promise.all([
-      Deno.writeTextFile("mod.js", denoOutput),
-      Deno.writeTextFile("index.cjs", cjsOutput),
-    ]);
-  }),
-
-  Deno.readTextFile("colors.js").then((input) => {
-    const output = '"use strict";\n\n' + input
-      .replaceAll("export const ", "exports.");
-
-    return Deno.writeTextFile("colors.cjs", output);
-  }),
+build([
+  "https://deno.land/x/postcss_value_parser@v4.1.0/mod.js",
+  "postcss-value-parser",
 ]);
